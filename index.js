6d92748910c50a9a0419cfd1fef954862c4e54fd
aritramullick <aritramullick@g.ucla.edu>
2022-10-29 15:26:20 -0700

    Initial commit

--- index.js
+++ index.js
@@ -20,3 +20,28 @@ const portNumber = process.env.PORT || 3000;
 app.listen(portNumber, function () {
     console.log("Managemate Backend listening on port " + portNumber);
 });
+
+
+
+async function addToGroup() {
+ // Checking if email exists 
+ const user = await roommateGroupSchema.findOne({email: req.body.email});
+ if(!user) return res.status(400).send('User is not registered');
+}
+
+class FormObject {
+  constructor(name, email, groupName) {
+    this.name = name;
+    this.email = email;
+    this.groupName = groupName;
+  }
+}
+
+function randomStr(len, arr) {
+  var ans = '';
+  for (var i = len; i > 0; i--) {
+      ans += 
+        arr[Math.floor(Math.random() * arr.length)];
+  }
+  return ans;
+}
