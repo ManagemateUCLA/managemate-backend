9ad027691b62f24fe50c99d19ecd2a7bcc1ec6c7
aritramullick <aritramullick@g.ucla.edu>
2022-10-29 17:12:33 -0700

    Middleware setup for roommategroupJS

--- routes/roommateGroup.js
+++ routes/roommateGroup.js
@@ -7,6 +7,10 @@ const { registerValidation, loginValidation } = require('../validation');
 const { response } = require('express');
 
 
+// declaring constant string
+const alphanumericArray = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+// helper function to create a string of length len
 function randomStr(len, arr) {
     var ans = '';
     for (var i = len; i > 0; i--) {
@@ -14,39 +18,60 @@ function randomStr(len, arr) {
           arr[Math.floor(Math.random() * arr.length)];
     }
     return ans;
-  }
-  
-  const alphanumericArray = "12345abcdefgh"
-  
+}
+
   async function createGroup(formObject) {
-    let gid = randomStr(5, alphanumericArray)
-   // Checking if email exists 
-   let roommateGroupExists = await roommateGroup.findOne({gid: gid});
-  
-   if(!roommateGroupExists) {
-    // this is a valid new GID 
-    let newRoommateGroup = new RoommateGroup({
-      gid: gid ,
-      name: formObject.groupName,
-      members: [],
-      discordlink: "aritra.com"
-    }); 
-  
-    try {
-      const savedGroup = await newRoommateGroup.save();
-      return gid;
-      } catch(err) {
-      return null;
-      }
-   }
-   return null;
+    const MAX_ATTEMPTS = 1000;
+    let tooManyRepeatedTries = 0;
+    
+    while (true) {
+        let gid = randomStr(5, alphanumericArray)
+        // Checking if email exists 
+        let roommateGroupExists = await roommateGroup.findOne({gid: gid});
+        
+        if(!roommateGroupExists) {
+            // this is a valid new GID 
+            let newRoommateGroup = new RoommateGroup({
+            gid: gid ,
+            name: formObject.groupName,
+            members: [],
+            discordlink: "aritra.com"
+            }); 
+        
+            try {
+            const savedGroup = await newRoommateGroup.save();
+            return gid;
+            } catch(err) {
+                return null;
+            }
+        }
+
+        else {
+            if (tooManyRepeatedTries >= MAX_ATTEMPTS) {
+                return null;
+            }
+            else {
+                tooManyRepeatedTries+=1;
+            }
+        }
+    }
+    return null;
   }
   
   router.post('/create', async (req, res) => {
-    console.log(req.body);
-    // some logic to create FormObjectfrom given req.body 
-    // call createGroup()
-    response = createGroup(formObject);
+    //    Expected Request Body
+    // {
+    //     email: "amarda@gmail.com",
+    //     groupName: "LF Group",
+    //     name: "Abhishek Marda",
+    //     uid: "_id"
+    //   }
+
+    let formObject = new FormObject(req.body);
+    if (!f) {
+        res.status(404).send("Bad Request Format: " + req.body)
+    }
+    let response = await createGroup(formObject);
     if (!response) {
         res.status(404).send("Invalid Group ID creation");
     }
@@ -58,10 +83,24 @@ function randomStr(len, arr) {
 
 
   class FormObject {
+
     constructor(name, email, groupName, uid) {
       this.name = name;
       this.email = email;
       this.groupName = groupName;
       this.uid = uid;
+    }
+
+    constructor(jsonBbody) {
+        let obj = JSON.parse(jsonBbody);
+        if (obj.name !== null && obj.email != null && obj.groupName != null && obj.uid != null) {
+            this.name = obj.name;
+            this.email = obj.email;
+            this.groupName = obj.groupName;
+            this.uid = obj.uid;
+        }
+        else {
+            this = null
+        }
     }
   }
