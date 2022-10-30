e0ccdc808a612aa50d1061bc476737d3bc08c882
AbhishekMarda <abhishek1marda@ucla.edu>
2022-10-29 17:17:40 -0700

    Completes join group API

--- model/User.js
+++ model/User.js
@@ -23,6 +23,9 @@ const userSchema = new mongoose.Schema(
         date: {
             type: Date,
             default: Date.now
+        },
+        gid: {
+            type: String
         }
     }
 );
--- routes/roommateGroup.js
+++ routes/roommateGroup.js
@@ -1,7 +1,48 @@
 // creating a router 
 const router = require('express').Router();
 const RoommateGroup = require('../model/RoommateGroup');
+const User = require('../model/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { registerValidation, loginValidation } = require('../validation')
 
+
+
+router.post('/join', verify, async (req, res) => {
+    /*
+        Expected fields in request body
+        {
+            'gid': String
+            'user name': String
+            'uid': String
+        }
+    */
+    const groupId = req.body.gid
+    const userId = req.body.uid
+    RoommateGroup.findOneAndUpdate(
+        {gid : groupId},
+        {"$push" : {members: uid}}, 
+        null,
+        (error, result) =>{
+            if (error) {
+                console.log(error)
+            } else if (!result) {
+                return res.status(400).send("Could not find roommate group!")
+            }
+        }
+    )
+    
+    User.findOneAndUpdate(
+        {_id: uid},
+        {gid: groupId},
+        null,
+        (error, result) =>{
+            if (error) {
+                console.log(error)
+            } else if (!result) {
+                return res.status(400).send("Could not find user!")
+            }
+        }
+    )    
+}
+)
