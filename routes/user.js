daf3cecaceb8e38952d7f1e64ec8893fe895cd33
aritramullick <aritramullick@g.ucla.edu>
2022-11-28 08:06:04 -0800

    perfecto for saving refresh token

--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,14 @@ const constants = require('../constants');
 const helpers = require('../helpers/general');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { response } = require('express');
+const {
+    response
+} = require('express');
 const verify = require("./verifyJWTToken");
 var axios = require('axios');
 var qs = require('qs');
 const gCal = require('../helpers/googleCal');
+let querystring = require('querystring');
 
 /**
  * * Inserts Google Calendar API token for the User
@@ -35,83 +38,72 @@ const gCal = require('../helpers/googleCal');
 
 // })
 
- router.post('/addToken', verify, async (req, res) => {
-    try{
-        
+router.post('/addToken', verify, async (req, res) => {
+    try {
         const code = req.body.code;
         const uid = req.user._id;
         const client_id = process.env.CLIENT_ID;
         const client_secret = process.env.CLIENT_SECRET;
         const redirect_uri = process.env.REDIRECT_URI;
-        
-        var data = qs.stringify({
-            'client_id': client_id,
-            'client_secret': client_secret,
-            'code': code,
-            'grant_type': 'authorization_code',
-            'redirect_uri': redirect_uri 
-          });
-          var config = {
-            method: 'post',
-            url: 'https://oauth2.googleapis.com/token',
-            headers: { 
-              'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data : data
-          };
-          
-          axios(config)
-          .then(function (response) {
-            let response_data = JSON.stringify(response.data);
-            console.log(response_data);
-            if (response_data.refresh_token !== null) {
-                try {
-                    let refresh_token = response_data.refresh_token;
-                    User.findOneAndUpdate(
-                        {_id: uid}, 
-                        {gcal_refresh_token: refresh_token},
-                        null,
-                        (error, result) => {
-                            if (error) {
-                                console.log(error)
-                                res.status(400).send(error)
-                                return;
-                            } else {
-                                console.log(result)
-                                res.status(200).send("Sucessfully added token");
-                                return;
-                            }
-                        }
-                    );
+        let data = {
+            client_id: client_id,
+            client_secret: client_secret,
+            code: code,
+            grant_type: 'authorization_code',
+            redirect_uri: redirect_uri
+        };
+        let url = 'https://oauth2.googleapis.com/token';
+        let response = await axios.post(url, querystring.stringify(data), {
+            headers: {
+                'content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        let response_data = response.data;
 
-                } catch(err) {
-                    res.status(400).send(err);
-                }
+        if (response_data.hasOwnProperty("refresh_token")) {
+
+            let refresh_token = response_data.refresh_token;
+            try {
+                let resp = await User.findOneAndUpdate({
+                    _id: uid
+                }, {
+                    gcal_refresh_token: refresh_token
+                }, );
+            } catch (err) {
+                res.status(400).send(err);
+                return;
             }
-            else {
-                res.status(400).send({"error": "wrong params or pre-existing refresh_token"});
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-            res.status(404).send(error);
-          });
-    }
-    catch(err) {
-        res.status(400).send(json({message: err}));
+            res.status(200).send("successfully added refresh token");
+            return;
+        } else {
+            res.status(404).send("no refresh token in body - already registered with google calendar");
+            return;
+        }
+
+    } catch (err) {
+        res.status(400).send("unknown connection error");
+        return;
     }
 });
 
 router.get('/checkUserInGroup', verify, async (req, res) => {
     try {
-        let userinfo = await User.findOne({_id: req.user._id});
+        let userinfo = await User.findOne({
+            _id: req.user._id
+        });
         let gid = userinfo.gid;
         if (!gid)
-            return res.status(200).send({gid: null});
+            return res.status(200).send({
+                gid: null
+            });
         console.log(gid);
-        return res.status(200).send({gid: gid});
-    } catch(err) {
-        return res.status(400).send({message: err});
+        return res.status(200).send({
+            gid: gid
+        });
+    } catch (err) {
+        return res.status(400).send({
+            message: err
+        });
     }
 });
 module.exports = router;
